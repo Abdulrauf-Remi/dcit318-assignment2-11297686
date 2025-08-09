f999f4fbd2ce48f8ae17691825235218b4d53d3c
Abdulrauf-Remi <anadeenara@gmail.com>
2025-08-09 11:35:57 +0000

    Update main Program.cs to organize and demonstrate all OOP concepts

--- OOPDemo/Program.cs
+++ OOPDemo/Program.cs
@@ -1,152 +1,29 @@
 ﻿using System;
+using OOPDemo.InheritanceandOverriding;
+using OOPDemo.AbstractClassesandMethods;
+using OOPDemo.Interface;
 
 namespace OOPDemo
 {
-    // 1. INHERITANCE AND METHOD OVERRIDING
-    // Base class Animal
-    public class Animal
-    {
-        public virtual void MakeSound()
-        {
-            Console.WriteLine("Some generic sound");
-        }
-    }
-
-    // Derived class Dog
-    public class Dog : Animal
-    {
-        public override void MakeSound()
-        {
-            Console.WriteLine("Bark");
-        }
-    }
-
-    // Derived class Cat
-    public class Cat : Animal
-    {
-        public override void MakeSound()
-        {
-            Console.WriteLine("Meow");
-        }
-    }
-
-    // 2. ABSTRACT CLASSES AND METHODS
-    // Abstract base class Shape
-    public abstract class Shape
-    {
-        public abstract double GetArea();
-    }
-
-    // Derived class Circle
-    public class Circle : Shape
-    {
-        private double radius;
-
-        public Circle(double radius)
-        {
-            this.radius = radius;
-        }
-
-        public override double GetArea()
-        {
-            return Math.PI * radius * radius;
-        }
-    }
-
-    // Derived class Rectangle
-    public class Rectangle : Shape
-    {
-        private double length;
-        private double width;
-
-        public Rectangle(double length, double width)
-        {
-            this.length = length;
-            this.width = width;
-        }
-
-        public override double GetArea()
-        {
-            return length * width;
-        }
-    }
-
-    // 3. INTERFACES
-    // Interface IMovable
-    public interface IMovable
-    {
-        void Move();
-    }
-
-    // Class Car implementing IMovable
-    public class Car : IMovable
-    {
-        public void Move()
-        {
-            Console.WriteLine("Car is moving");
-        }
-    }
-
-    // Class Bicycle implementing IMovable
-    public class Bicycle : IMovable
-    {
-        public void Move()
-        {
-            Console.WriteLine("Bicycle is moving");
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("DCIT 318: PROGRAMMING II ASSIGNMENT 2");
             Console.WriteLine("Object-Oriented Programming Demonstrations");
+            Console.WriteLine("Student ID: 11297686");
             Console.WriteLine("==========================================\n");
 
-            // 1. INHERITANCE AND METHOD OVERRIDING DEMONSTRATION
-            Console.WriteLine("1. INHERITANCE AND METHOD OVERRIDING");
-            Console.WriteLine("-----------------------------------");
-            
-            Animal animal = new Animal();
-            Dog dog = new Dog();
-            Cat cat = new Cat();
+            // Demonstrate Inheritance and Overriding
+            InheritanceDemo.DemonstrateInheritance();
 
-            Console.WriteLine("Animal sound:");
-            animal.MakeSound();
-            
-            Console.WriteLine("Dog sound:");
-            dog.MakeSound();
-            
-            Console.WriteLine("Cat sound:");
-            cat.MakeSound();
-            Console.WriteLine();
+            // Demonstrate Abstract Classes and Methods
+            AbstractDemo.DemonstrateAbstractClasses();
 
-            // 2. ABSTRACT CLASSES AND METHODS DEMONSTRATION
-            Console.WriteLine("2. ABSTRACT CLASSES AND METHODS");
-            Console.WriteLine("-------------------------------");
-            
-            Circle circle = new Circle(5.0);
-            Rectangle rectangle = new Rectangle(4.0, 6.0);
+            // Demonstrate Interfaces
+            InterfaceDemo.DemonstrateInterfaces();
 
-            Console.WriteLine($"Circle with radius 5.0: Area = {circle.GetArea():F2}");
-            Console.WriteLine($"Rectangle with length 4.0 and width 6.0: Area = {rectangle.GetArea():F2}");
-            Console.WriteLine();
-
-            // 3. INTERFACES DEMONSTRATION
-            Console.WriteLine("3. INTERFACES");
-            Console.WriteLine("-------------");
-            
-            Car car = new Car();
-            Bicycle bicycle = new Bicycle();
-
-            Console.WriteLine("Car movement:");
-            car.Move();
-            
-            Console.WriteLine("Bicycle movement:");
-            bicycle.Move();
-            Console.WriteLine();
-
+            Console.WriteLine("All OOP concepts demonstrated successfully!");
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
